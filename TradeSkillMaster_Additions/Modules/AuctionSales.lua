999d7b4c5c5a372b97397687678cab0ab6c2be90
Skyler Hiscock <skylerhiscock@gmail.com>
2021-08-23 00:08:12 -0500

    proper way of retrieving itemLink

--- TradeSkillMaster_Additions/Modules/AuctionSales.lua
+++ TradeSkillMaster_Additions/Modules/AuctionSales.lua
@@ -25,7 +25,8 @@ function AuctionSales:AUCTION_OWNED_LIST_UPDATE()
 	for i = 1, GetNumAuctionItems("owner") do
 		local name, _, quantity, _, _, _, _, _, buyout, _, _, _, wasSold = GetAuctionItemInfo("owner", i)
 		if wasSold == 0 and (buyout and buyout > 0) then
-			TSM.db.char.auctions[name] = {buyout=buyout, quantity=quantity}
+			local itemLink = GetAuctionItemLink("owner", i)
+			TSM.db.char.auctions[name] = {buyout=buyout, quantity=quantity, itemLink=itemLink}
 		end
 	end
 end
@@ -36,19 +37,15 @@ local EXPIRED = ERR_AUCTION_EXPIRED_S:gsub("%%s", "(.+)")
 function AuctionSales.FilterSystemMsg(chatFrame, event, msg, ...)
 	local expiredItem = strmatch(msg, EXPIRED)
 	local soldItem = strmatch(msg, SOLD)
-	local itemName = expiredItem or soldItem
 
-	local auctionItemInfo = TSM.db.char.auctions[itemName]
+	local auctionItemInfo = TSM.db.char.auctions[expiredItem or soldItem]
 	if not auctionItemInfo then return end
 
-	local _, itemLink = GetItemInfo(itemName)
-	if not itemLink then return end
-
 	if expiredItem then
-		return nil, format(ERR_AUCTION_EXPIRED_S, itemLink), ...
+		return nil, format(ERR_AUCTION_EXPIRED_S, auctionItemInfo.itemLink), ...
 	end
 
 	if soldItem then
-		return nil, format(L["Your auction of %sx%s has sold for |cFFFFFFFF%s|r"], itemLink, auctionItemInfo.quantity, GetCoinTextureString(auctionItemInfo.buyout)), ...
+		return nil, format(L["Your auction of %sx%s has sold for |cFFFFFFFF%s|r"], auctionItemInfo.itemLink, auctionItemInfo.quantity, GetCoinTextureString(auctionItemInfo.buyout)), ...
 	end
 end
